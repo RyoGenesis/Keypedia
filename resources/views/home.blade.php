e91d01e0ee4b9a0318b06046fc54fefa083f3841
RyoGenesis <rtunggala18@gmail.com>
2021-12-06 00:02:10 +0700

    added header and footer

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('sharedLayout.layout')
+@section('title-header','Keypedia')
+
+@section('content')
+    <div class="row">
+        <div class="col-12 text-center">
+            <h1>Welcome To Keypedia</h1>
+            <p class="fw-bold fs-6">Best Keyboard and Keycaps Shop</p>
+        </div>
+    </div>
+@endsection
